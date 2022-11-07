36e39478a8e17fc02f460bf8cebe1a94c37d5321
Admin <Admin@USERTL16.tpsonline.com>
2022-11-07 13:16:05 +0500

    second commit

--- index.js
+++ index.js
@@ -1,43 +1,9 @@
-const e = require('express');
 const express = require('express')
 const app = express()
 const port = 3000
-const prompt = require('prompt');
-prompt.start();
 
 app.get('/', (req, res) => {
-
-    let text = 'umer'
-    let key = 1
-
-    if (text.toLowerCase() === text){
-        console.log("this is lowercase")
-    }
-    else
-    {
-        console.log("this is uppercase")
-    }
-    
-    function cipher(text, key){
-        let result = ''
-        for (let i = 0; i < text.length; i++)
-        {
-            let char = text[i];
-            if (char.toUpperCase(text[i]))
-            {
-                let ch =  String.fromCharCode((char.charCodeAt(0) + key-65) % 26 + 65);
-                result += ch;
-            }
-            else
-            {
-                let ch = String.fromCharCode((char.charCodeAt(0) + key-97) % 26 + 97);
-                result += ch;
-            }
-        }
-        return result;
-    }
-
-    console.log(cipher('umer',1))
+  res.send('Hello World!')
 })
 
 app.listen(port, () => {
