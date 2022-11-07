a4322246c0921f6621bb646382f22053f16c4c26
Admin <Admin@USERTL16.tpsonline.com>
2022-11-07 13:56:12 +0500

    fourth commit

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
+const { response } = require('express')
 const express = require('express')
 const app = express()
 const port = 3000
+const pool = require('./dbConn')
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+
+app.get('/', (request, response) => {
+  response.json({info : 'Node.js,Express, and Postgres API'})
 })
+
+app.get('/testdb', async(request, response) => {
+    let res = await pool.query('select * from public.todoList')
+    console.log(res);
+    response.json({info : 'Node.js,Express, and Postgres API'})
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
